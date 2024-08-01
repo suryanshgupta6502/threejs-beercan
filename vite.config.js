a2a082568a51007adb94d74a2045fad01eb5a233
Suryansh <suryanshgupta6502@gmail.com>
2024-08-01 15:48:43 +0530

    remove vercel.json

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
+
+export default {
+    
+    root: './',
+    publicDir: "./public/",
+    base: './',
+    
+    
+
+}
